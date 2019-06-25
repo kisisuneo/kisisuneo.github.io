4a004658c0752507c6e4b67c081774a6d20aba97
kisisuneo <kisisuneo@users.noreply.github.io>
2019-06-25 17:13:15 +0700

    New
    nothing new

---
+++ lightbulb.js
@@ -0,0 +1,19 @@
+		document.write("This is hello button <br/>");
+		function sayBye(a, b){
+            console.log (a + " is " + b + " years old.");
+         }
+
+        function mon(a,b){
+        	var c=a+b;
+        	var d=50;
+        	console.log(c);
+        	for(c; c<d;c++){
+        		console.log(c);
+        	}
+        }
+        function direct(){
+        	window.location="https://kisisuneo.github.io/bai9.html";
+        	alert("You will be redirected to main page in 10 sec.");
+            setTimeout('Redirect()', 10000);
+        }
+
